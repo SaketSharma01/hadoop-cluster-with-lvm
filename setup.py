cdb18c674529fc2340acc1f0d34f0a032ad46333
Saket Sharma <69979381+saket-cmd@users.noreply.github.com>
2020-12-13 01:26:12 +0530

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,26 @@
+import os
+import lvm
+import hadoop
+os.system("tput setaf 3")
+print(\t"************************************************")
+print(" \t\tPress 1: To setup hadoop \n\t\t Press 2: To setup lvm")
+try:
+  ch=int("Enter ur choice :")
+  if ch==1:
+    while True:
+      hadoop.hadoop()
+  elif ch==2:
+     print("\t\tPress 1:For remotr setup \n\t\tPress 2: For local setup
+     ch1=int("Enter ur choice :")
+     if ch1==1:
+       lvm.lvmremote()
+     elif ch1==2:
+       lvm.lvmlocal()
+     else:
+       print("Doesn't support")
+  else:
+     print("Doesn't support")
+except ValueError:
+  print("Please enter correct input")
+  os.system("tput setaf 7")
+   
