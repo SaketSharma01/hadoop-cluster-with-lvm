17e36bbca88869a0e53d92f30be168f1f6cc3c30
Saket Sharma <69979381+saket-cmd@users.noreply.github.com>
2020-12-13 02:31:20 +0530

    Update lvm.py

--- lvm.py
+++ lvm.py
@@ -1,9 +1,10 @@
 import os
 import getpass
 def lvmremote():
-	ip=input("Enter the required ip address: ")
-	ps=getpass.getpass("Enter {ip}'s password: ")
-	print(''' press 1: to show hard disks
+    ip=input("Enter the required ip address: ")
+    ps=getpass.getpass(f"Enter {ip}'s password: ")
+    while True:
+	print(''' \tpress 1: to show hard disks
 	press 2: to create physical volume
 	press 3: to display physical volume
 	press 4: to create volume group
@@ -19,57 +20,57 @@ def lvmremote():
 	press 14: exit''')
 	ch=int(input("Enter ur choice: "))
 	if ch==1:
-		os.system(f"sshpass {ps} ssh {ip} fdisk -l")
+		os.system(f"sshpass -p {ps} ssh {ip} fdisk -l")
 	elif ch==2:
 		diskname=input("Enter the disk name: ")
-		os.system(f"sshpass {ps} ssh {ip}pvcreate {diskname}")
+		os.system(f"sshpass -p {ps} ssh {ip} pvcreate {diskname}")
 	elif ch==3:
 		diskname=input("Enter the disk name: ")
-		os.system(f"sshpass {ps} ssh {ip} pvdisplay {diskname}")
+		os.system(f"sshpass -p {ps} ssh {ip} pvdisplay {diskname}")
 	elif ch==4:
 		vgname=input("Enter the volume group name: ")
 		disknames=input("Enter the diskname separated by a space").split(' ')
 		new=' '.join(str(x) for x in disknames)
-		os.system(f"sshpass {ps} ssh {ip} vgcreate {vgname} {new}")
+		os.system(f"sshpass -p {ps} ssh {ip} vgcreate {vgname} {new}")
 	elif ch==5:
 		vgname=input("Enter the volume group name: ")
-		os.system(f"sshpass {ps} ssh {ip} vgdisplay {vgname}")
+		os.system(f"sshpass -p {ps} ssh {ip} vgdisplay {vgname}")
 	elif ch==6:
 		lvname=input("Enter the logical volume name: ")
 		vgname=input("Enter the volume group name: ")
 		size=input("Enter the logical volume size: ")
-		os.system(f"sshpass {ps} ssh {ip} lvcreate --size {size} --name {lvname}  {vgname}")
+		os.system(f"sshpass -p {ps} ssh {ip} lvcreate --size {size} --name {lvname}  {vgname}")
 	elif ch==7:
 		lvname=input("Enter the logical volume name: ")
 		vgname=input("Enter the volume group name: ")
-		os.system(f"sshpass {ps} ssh {ip} lvdisplay {vgname}/{lvname} ")
+		os.system(f"sshpass -p {ps} ssh {ip} lvdisplay {vgname}/{lvname} ")
 	elif ch==8:
 		lvname=input("Enter the logical volume name: ")
 		vgname=input("Enter the volume group name: ")
-		os.system(f"sshpass {ps} ssh {ip} mkfs.ext4 /dev/{vgname}/{lvname} ")
+		os.system(f"sshpass -p {ps} ssh {ip} mkfs.ext4 /dev/{vgname}/{lvname} ")
 	elif ch==9:
 		folder=input("Enter the folder name: ")
 		lvname=input("Enter the logical volume name: ")
 		vgname=input("Enter the volume group name: ")
-		os.system(f"sshpass {ps} ssh {ip} mkdir /{folder} ; mount /dev/{vgname}/{lvname} /{folder} ; df -hT")
+		os.system(f"sshpass -p {ps} ssh {ip} mkdir /{folder} ; mount /dev/{vgname}/{lvname} /{folder} ; df -hT")
 	elif ch==10:
 		lvname=input("Enter the logical volume name: ")
 		vgname=input("Enter the volume group name: ")
 		esize=input("Enter the extending size: ")
-		os.system(f"sshpass {ps} ssh {ip} lvextend --size +{esize}  /dev/{vgname}/{lvname} ; resize2fs /dev/{vgname}/{lvname} ")
+		os.system(f"sshpass -p {ps} ssh {ip} lvextend --size +{esize}  /dev/{vgname}/{lvname} ; resize2fs /dev/{vgname}/{lvname} ")
 	elif ch==11:
 		vgname=input("Enter the volume group name: ")
 		pvname=input("Enter the new physical volume nme: ")
-		os.system(f"sshpass {ps} ssh {ip} vgextend {vgname}  {pvname} ")
+		os.system(f"sshpass -p {ps} ssh {ip} vgextend {vgname}  {pvname} ")
 	elif ch==12:
 		rsize=input("Enter the reduced size here: ")
 		folder=input("Enter the folder name: ")
 		lvname=input("Enter the logical volume name: ")
 		vgname=input("Enter the volume group name: ")
-		os.system(f"sshpass {ps} ssh {ip} umount /{folder} ; e2fsck -f /dev/{vgname}/{lvname} ; resize2fs /dev/{vgname}/{lvname} {rsize} ")
-		os.system(f"sshpass {ps} ssh {ip} lvreduce --size {rsize} /dev/{vgname}/{lvname} ; mount /dev/{vgname}/{lvname}  /{folder} ")
+		os.system(f"sshpass -p {ps} ssh {ip} umount /{folder} ; e2fsck -f /dev/{vgname}/{lvname} ; resize2fs /dev/{vgname}/{lvname} {rsize} ")
+		os.system(f"sshpass -p {ps} ssh {ip} lvreduce --size {rsize} /dev/{vgname}/{lvname} ; mount /dev/{vgname}/{lvname}  /{folder} ")
 	elif ch==13:
-		os.system(f"sshpass {ps} ssh {ip} lvs")
+		os.system(f"sshpass -p {ps} ssh {ip} lvs")
 	elif ch==14:
 		break
   else:
